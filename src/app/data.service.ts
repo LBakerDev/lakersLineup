c9775669d7307c5db375946f4364f272d18a53c9
londonbaker4@gmail.com <londonbaker4@gmail.com>
2018-03-06 14:58:46 -0500

    removing unused code

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs/Subject";
-import "rxjs/add/observable/of";
-import { Observable } from "rxjs/Observable";
-import { of } from "rxjs/observable/of";
 import { Http } from "@angular/http";
 import "rxjs/add/operator/map";
 
-import { RESERVES } from "./RESERVES";
 
 @Injectable()
 export class DataService {
-  public newReserveSubject = new Subject<any>();
   constructor(private http: Http) {}
 
   // Http request to retrieve starters
